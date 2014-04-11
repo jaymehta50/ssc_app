ef5d6fe936ff7172a1bc6d92ecba8f85e1dbf10e
Jay Mehta <jay.mehta@hotmail.co.uk>
2014-04-11 16:18:36 +0100

    Fixed some bugs in last commit

--- application/models/problems_model.php
+++ application/models/problems_model.php
@@ -9,11 +9,13 @@ class Problems_model extends CI_Model {
 
 	public function getadultprob()
 	{
+		/*
 		$this->db->distinct();
 		$this->db->select('clinical_problem');
 		$this->db->order_by('clinical_problem','asc');
 		$query = $this->db->get('problem_list_adult');
 		return $query->result_array();
+		*/
 	}
 
 	public function getadultprobnames()
@@ -25,7 +27,7 @@ class Problems_model extends CI_Model {
 	public function getoneadultprob($problem)
 	{
 		$this->db->order_by('problem_subgroup','asc');
-		$this->db->where("lower(clinical_problem) = '".str_replace("_", " ", $problem)."'");
+		$this->db->where('clinical_problem_id', $problem);
 		$query = $this->db->get('problem_list_adult');
 		return $query->result_array();
 	}
