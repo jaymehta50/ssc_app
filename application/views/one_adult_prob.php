0be2cc22d76945aca1d1989fee483ca6e13c1dc3
Jay Mehta <jay.mehta@hotmail.co.uk>
2014-04-12 19:07:48 +0100

    Changed subproblem views from article to section

--- application/views/one_adult_prob.php
+++ application/views/one_adult_prob.php
@@ -9,7 +9,7 @@
         <article id="one_adult_prob" class="list scroll active">
             <ul class="intended">
                 <li><p>These are the subgroups of problems associated with <span style="font-style:italic;"><?php echo $problem_name['clinical_problem']; ?></span></p>
-                <p>Click on one to see the condition(s) associated with it!</p>
+                <p>Click on one to see the condition(s) associated with it!</p></li>
             </ul>
             <ul class='indenteds'>
                 <?php
@@ -20,32 +20,34 @@
                     {
                         if($prev==$value['problem_subgroup']) continue;
                         $i += 1;
-                        $subgroup_array[] = $value['problem_subgroup'];
-                        echo '<li class="selectable"><a href="#" data-view-article="subprob_'.$i.'"><strong>'.$value['problem_subgroup'].'</strong></a></li>';
+                        $subgroup_array[] = array('problem_id' => $value['clinical_problem_id'], 'subprob' => $value['problem_subgroup'], 'subprob_no' => $i);
+                        echo '<li class="selectable"><a href="#" data-view-section="adultprob_'.$value['clinical_problem_id'].'_subprob_'.$i.'"><strong>'.$value['problem_subgroup'].'</strong></a></li>';
                         $prev = $value['problem_subgroup'];
                     }
                 ?>
             </ul>
         </article>
+    </section>
 
         <?php
-        $i = 0;
         foreach($subgroup_array as $value) {
-            $i += 1;
-            echo '<article id="subprob_'.$i.'" class="list indenteds scroll"><ul class="intended">
-                <li><p>These are the conditions associated with <span style="font-style:italic;">'.$problem_name['clinical_problem'].' - '.$value.'</span></p>
-                <p>Click on a condition to see when in the course it appears and to make notes on it!</p>
+            echo '<section id="adultprob_'.$value['problem_id'].'_subprob_'.$value['subprob_no'].'" data-transition="slide" data-aside="features" class="drag">
+        <header data-back="chevron-left">'.$problem_name['clinical_problem'].' - '.$value['subprob'].'
+            <nav class="on-right">
+                <a href="#"><abbr class="text tiny ">v0.1</abbr></a>
+            </nav>
+        </header><article id="subprob_'.$value['subprob_no'].'" class="list scroll active"><ul class="intended">
+                <li><p>These are the conditions associated with <span style="font-style:italic;">'.$problem_name['clinical_problem'].' - '.$value['subprob'].'</span></p>
+                <p>Click on a condition to see when in the course it appears and to make notes on it!</p></li>
             </ul><ul class="intendeds">';
             foreach($one_adult_prob as $value2)
             {
                 if($value!=$value2['problem_subgroup']) continue;
                 echo '<li class="selectable"><a href="#" data-view-article=""><strong>'.$value2['condition'].'</strong></a></li>';
             }   
-            echo '</ul></article>';
+            echo '</ul></article></section>';
         }
 
         ?>
-
-    </section>
 
 
