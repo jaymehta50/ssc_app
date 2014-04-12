cb45524f7728c93acb4c947364be6a72ebed0d24
Jay Mehta <jay.mehta@hotmail.co.uk>
2014-04-12 19:25:25 +0100

    Third try to fix backwards navigation buttons

--- application/views/one_adult_prob.php
+++ application/views/one_adult_prob.php
@@ -1,6 +1,8 @@
 <section id="adult_prob_<?php echo $one_adult_prob[0]['clinical_problem_id']; ?>" data-transition="slide" data-aside="features" class="drag">
         <header>
-            <a href="#back" data-view-section="back" data-icon="chevron-left"></a>
+            <nav>
+                <a href="#back" data-view-section="back" data-icon="chevron-left"></a>
+            </nav>
             <?php echo $problem_name['clinical_problem']; ?>
             <nav class="on-right">
                 <a href="#"><abbr class="text tiny ">v0.1</abbr></a>
@@ -33,7 +35,7 @@
         <?php
         foreach($subgroup_array as $value) {
             echo '<section id="adultprob_'.$value['problem_id'].'_subprob_'.$value['subprob_no'].'" data-transition="slide" data-aside="features" class="drag">
-        <header><a href="#back" data-view-section="back" data-icon="chevron-left"></a>'.$problem_name['clinical_problem'].' - '.$value['subprob'].'
+        <header><nav><a href="#back" data-view-section="back" data-icon="chevron-left"></a></nav>'.$problem_name['clinical_problem'].' - '.$value['subprob'].'
             <nav class="on-right">
                 <a href="#"><abbr class="text tiny ">v0.1</abbr></a>
             </nav>
