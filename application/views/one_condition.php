9458834595e0c4d4595e78e7061cb689000615c7
Jay Mehta <jay.mehta@hotmail.co.uk>
2014-04-16 16:08:40 +0100

    Debug of one-condition.php

--- application/views/one_condition.php
+++ application/views/one_condition.php
@@ -23,23 +23,3 @@
             </ul>
         </article>
     </section>
-
-        <?php
-        foreach($subgroup_array as $value) {
-            echo '<section id="adultprob_'.$value['problem_id'].'_subprob_'.$value['subprob_no'].'" data-transition="slide" data-aside="features" class="drag">
-        <header><nav><a href="#back" data-view-section="back"><span class="icon chevron-left"></span></a></nav>';
-        if(($_COOKIE['devwidth'] - 42) <= (strlen($problem_name['clinical_problem'].': '.$value['subprob']) * $av_char_width)) echo "<marquee behavior='alternate' scrollamount='2' style='padding-right:7px;'>".$problem_name['clinical_problem'].': '.$value['subprob']."</marquee>";
-        else echo $problem_name['clinical_problem'].': '.$value['subprob'];
-        echo '</header><article id="subprob_'.$value['subprob_no'].'" class="list scroll active"><div class="indented"><ul>
-                <li><p>These are the conditions associated with:</p><div style="font-style:italic;text-align:center;margin:9px 0 10px 0;"><p>'.$problem_name['clinical_problem'].': '.$value['subprob'].'</p></div>
-                <p>Click on a condition to see when in the course it appears and to make notes on it!</p></li>
-            </ul></div><div class="intendeds"><ul>';
-            foreach($one_adult_prob as $value2)
-            {
-                if($value['subprob']!=$value2['problem_subgroup']) continue;
-                echo '<a href="#" data-view-article=""><li class="selectable arrow"><strong>'.$value2['condition'].'</strong></li></a>';
-            }   
-            echo '</ul></div></article></section>';
-        }
-
-        ?>
