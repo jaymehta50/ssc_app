a5442096c73b3ff15bd0a91fcbd48e457d92510c
Jay Mehta <jay.mehta@hotmail.co.uk>
2014-04-17 14:48:29 +0100

    Fixed bug in condition view

--- application/views/one_condition.php
+++ application/views/one_condition.php
@@ -15,7 +15,7 @@
                     <p>You are currently looking at:</p><div style="font-style:italic;text-align:center;margin:9px 0 10px 0;"><p><?php echo $condition['condition']; ?></p></div>
                     <p>You can review when in the course you would be expected to learn about this condition, add your own notes, and see where else in the Problem List this condition appears.</p>
                 </li>
-                <a href="#" id="condition_reveal(<?php echo $condition['id']; ?>)"><li><div><span class="icon chevron-down"></span><h2>Course Progress</h2></div></li></a>
+                <a href="#" id="condition_reveal_<?php echo $condition['id']; ?>"><li><div><span class="icon chevron-down"></span><h2>Course Progress</h2></div></li></a>
                 <li><div id="condition_<?php echo $condition['id']; ?>_course" style="display:none;"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam posuere varius libero, vel aliquet velit tempus et. Curabitur ac sollicitudin neque. Duis vel ipsum lobortis, gravida urna et, convallis purus. Sed porttitor vehicula erat, a tempus nisi mattis in. Aenean pulvinar vestibulum lacus, at egestas orci egestas eget. Aliquam lobortis ipsum sed risus commodo, vitae fermentum ipsum ultrices. Maecenas gravida lorem non velit dictum convallis.</p></div></li>
 
             </ul>
