78cc8df5fcda2c602467a4fc1154357c54f5cca4
Jay Mehta <jay.mehta@hotmail.co.uk>
2014-04-18 18:11:04 +0100

    Debug again of AJAX

--- application/views/notes.php
+++ application/views/notes.php
@@ -1,4 +1,4 @@
-<ul><li><p>You are currently looking at your notes on:</p><div style="font-style:italic;text-align:center;margin:9px 0 10px 0;"><p><?php echo $condition['condition']; ?></p></div><p>You can also see your notes for conditions with the same name below:</p><h2>My Notes</h2></li><?php
+<?php echo '<ul><li><p>You are currently looking at your notes on:</p><div style="font-style:italic;text-align:center;margin:9px 0 10px 0;"><p>'.$condition['condition'].'</p></div><p>You can also see your notes for conditions with the same name below:</p><h2>My Notes</h2></li>';
 if(!$notes) echo "<li><p>You do not have any notes on this condition at the moment</p></li>";
 else
 {
@@ -7,4 +7,4 @@ else
         echo "<li><div class='my_note'><p>".$value['note']."</p></div></li>";
     }
 }
-?></ul>
+echo '</ul>';
