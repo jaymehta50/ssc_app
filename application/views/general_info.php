f7cfad89a653a817fa7e0aebb22938d69843c67d
Jay Mehta <jay.mehta@hotmail.co.uk>
2014-04-22 16:04:39 +0100

    Minor styling changes

--- application/views/general_info.php
+++ application/views/general_info.php
@@ -12,16 +12,16 @@
             <a href="#" data-view-article="info3" data-label="Colour Ratings"></a>
         </nav>
 
-        <article id="info1" class="list scroll intended active">
+        <article id="info1" class="list scroll intendeds active">
             <ul>
                 <li>
-                    <strong class='text bold'>Introduction</strong>
+                    <strong class='text bold'>Introduction</strong><br />
                     <p class='text'>The ‘Clinical Problems and Conditions’ list has been developed as a resource to help students keep track of their clinical learning. It is an adaptation (with kind permission) of original material developed and published by Dr Henry Mandin. We are grateful for permission to use his earlier work.</p>
                 </li>
                 <li>
-                    <p class='text italic'>Refs:</p>
-                    <p class='text italic'>1. "Objectives for the qualifying examination" of the Medical Council of Canada, Second edition, 1999, H Mandin (Ed) [Third edition published 2003, Ed H Mandin, but the ‘problems and conditions list’ adaptation was derived from the 2nd edition]</p>
-                    <p class='text italic'>2.  H Mandin, P Harasym, C Eagle and M Watanabe : Developing a "clinical presentation"curriculum at the University of Calgary.": Acad-Med. 1995 Mar; 70 (3): 186-93.</p>
+                    <p class='text italic'>Refs:</p><br />
+                    <p class='text italic'>1. "Objectives for the qualifying examination" of the Medical Council of Canada, Second edition, 1999, H Mandin (Ed) [Third edition published 2003, Ed H Mandin, but the ‘problems and conditions list’ adaptation was derived from the 2nd edition]</p><br />
+                    <p class='text italic'>2.  H Mandin, P Harasym, C Eagle and M Watanabe : Developing a "clinical presentation"curriculum at the University of Calgary.": Acad-Med. 1995 Mar; 70 (3): 186-93.</p><br />
                     <p class='text italic'>3. Mandin H, Dauphinee WD "Conceptual guidelines for developing and maintaining curriculum and examination objectives: the experience of the Medical Council of Canada.": Acad-Med. 2000 Oct; 75 (10) : 1031-7</p>
                 </li>
                 <li>
@@ -30,16 +30,16 @@
             </ul>
         </article>
 
-        <article id="info2" class="list scroll intended">
+        <article id="info2" class="list scroll intendeds">
             <ul>
                 <li>
-                    <strong class='text bold'>About the Problem List</strong>
-                    <p class='text'>The list is composed of <span class='bold'>‘Clinical problems’</span> with which patients may present. Each Clinical problem is associated with a list of possible <span class='bold'>‘conditions’</span> that may cause it.</p><br />
-                    <p class='text'>Conditions are further divided into <span class='bold'>‘core conditions’</span> – those which are more common or important; and <span class='bold'>'non-core conditions'</span>, which are less so, but nevertheless important to be aware of.</p><br />
+                    <strong class='text bold'>About the Problem List</strong><br />
+                    <p class='text'>The list is composed of <span class='text bold'>‘Clinical problems’</span> with which patients may present. Each Clinical problem is associated with a list of possible <span class='text bold'>‘conditions’</span> that may cause it.</p><br />
+                    <p class='text'>Conditions are further divided into <span class='text bold'>‘core conditions’</span> – those which are more common or important; and <span class='text bold'>'non-core conditions'</span>, which are less so, but nevertheless important to be aware of.</p><br />
                     <p class='text'>There are separate lists for Clinical problems and conditions for adults and children. It has recently been updated and is now (November 2012) ready for use.</p>
                 </li>
                 <li>
-                    <strong class='text bold'>How to use the list</strong>
+                    <strong class='text bold'>How to use the list</strong><br />
                     <p class='text'>We think there are 2 main ways to use the list, though you will doubtless find others:</p><br />
                     <p class='text'>1. As a tool to guide reading around problems seen, focusing more (though not exclusively) on the core conditions – ‘what should I read about having seen this clinical problem?’</p><br />
                     <p class='text'>2. As a  ‘map’ to review  how one’s learning is progressing – ‘which of these problems have I seen or learned about and which do  I need to address to cover the gaps?’</p>
@@ -54,10 +54,10 @@
             </ul>
         </article>
 
-        <article id="info3" class="list scroll intended">
+        <article id="info3" class="list scroll intendeds">
             <ul>
                 <li>
-                    <strong class='text bold'>Red/Amber/Green Ratings</strong>
+                    <strong class='text bold'>Red/Amber/Green Ratings</strong><br />
                     <p class='text'>The list has been reviewed by a wide range of local clinicians from a range of specialties to minimise errors and maximise its relevance for day to day practice. Each attachment in the clinical course has rated how likely you are to come across each <span class="bold">clinical problem</span> in that part of the course.</p><br />
                     <table class="course">
                         <tbody>
