cb9422c110d0cac3d7cf391f060a1a21cdfbe134
Jay Mehta <jay.mehta@hotmail.co.uk>
2014-05-21 15:17:39 +0100

    Changed URL location

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://jkm50.user.srcf.net/ssc_app/';
+$config['base_url']	= 'http://jkm50.user.srcf.net/ssc/ssc_app/';
 
 /*
 |--------------------------------------------------------------------------
